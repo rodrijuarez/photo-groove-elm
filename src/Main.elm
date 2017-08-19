fbdf2b4d8c6d6bbfb13f1993125428e4b074677d
Rodrigo Juarez <rodrigo.juarez.inf@gmail.com>
2017-08-19 17:02:24 -0300

    Enhancement - Added thumbnail selection

--- src/Main.elm
+++ src/Main.elm
@@ -15,20 +15,23 @@ urlPrefix =
 
 
 initialModel =
-    [ { url = "1.jpeg" }
-    , { url = "2.jpeg" }
-    , { url = "3.jpeg" }
-    ]
+    { photos =
+        [ { url = "1.jpeg" }
+        , { url = "2.jpeg" }
+        , { url = "3.jpeg" }
+        ]
+    , selected = "1.jpeg"
+    }
 
 
-viewThumbnail thumbnail =
-    img [ src (urlPrefix ++ thumbnail.url) ] []
+viewThumbnail selectedThumbnail thumbnail =
+    img [ src (urlPrefix ++ thumbnail.url), classList [ ( MainCss.Selected, selectedThumbnail == thumbnail.url ) ] ] []
 
 
 view model =
     div [ class [ MainCss.Content ] ]
         [ h1 [] [ Html.text "Photo Groove" ]
-        , div [ id "thumbnails" ] (List.map viewThumbnail model)
+        , div [ id "thumbnails" ] (List.map (\photo -> viewThumbnail model.selected photo) model.photos)
         ]
 
 
